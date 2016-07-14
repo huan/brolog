6e5f36f3e2349a4f189888f2465d8942fab6c91a
Zhuohuan LI <zixia@zixia.net>
2016-07-14 22:22:17 +0800

    add static typescript define

--- index.d.ts
+++ index.d.ts
@@ -8,4 +8,12 @@ export declare class Brolog {
   info(prefix: string, ...args: any[]): any
   verbose(prefix: string, ...args: any[]): any
   silly(prefix: string, ...args: any[]): any
+
+  static level(levelName?: string): number
+
+  static error(prefix: string, ...args: any[]): any
+  static warn(prefix: string, ...args: any[]): any
+  static info(prefix: string, ...args: any[]): any
+  static verbose(prefix: string, ...args: any[]): any
+  static silly(prefix: string, ...args: any[]): any
 }
