d235a86d211e03041e807a0a4c55edbbb711f7e0
Zhuohuan LI <zixia@zixia.net>
2016-07-14 20:26:14 +0800

    add example

---
+++ example/npm-like-logger.js
@@ -0,0 +1,33 @@
+const log = require('..')
+
+function demo() {
+
+  console.log('#### Set Level to SILLY ####')
+  log.level('SILLY')
+  doLog()
+
+  log.level('INFO')
+  console.log('#### Set Level to INFO ####')
+  doLog()
+
+  console.log('#### Set Level to ERR ####')
+  log.level('ERR')
+  doLog()
+
+  console.log('#### Set Level to SILENT ####')
+  log.level('SILENT')
+  doLog()
+
+  console.log('#### BroLog Test Done ####')
+
+}
+
+function doLog() {
+  log.error('LogApp', 'error')
+  log.warn('LogApp', 'warn')
+  log.info('LogApp', 'info')
+  log.verbose('LogApp', 'verbose')
+  log.silly('LogApp', 'silly')
+}
+
+demo()
