2f2089ddcf780d8e7244ebca3e0808ea3f866744
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-10-26 16:18:34 +0800

    add loglevel type

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,21 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 // https://github.com/Microsoft/TypeScript/issues/2076#issuecomment-75052599
 
+export type LogLevel = 'silent'
+                      | 'error'
+                      | 'warn'
+                      | 'info'
+                      | 'verbose'
+                      | 'silly'
+
 interface Brolog {
-  (level?: string): Brolog
-  new (level?: string): Brolog
+  (level?: LogLevel): Brolog
+  new (level?: LogLevel): Brolog
   Brolog: Brolog
   // factory(name?: string): Brolog
 
-  level(name?: string): string
-  defaultLevel(name?: string): string
+  level(name?: LogLevel): LogLevel
+  defaultLevel(name?: LogLevel): LogLevel
 
   error(prefix: string, ...args: any[]): void
   warn(prefix: string, ...args: any[]): void
