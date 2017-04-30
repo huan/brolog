6a91d3cc1404a270c4128c10fa44c1d3d53f1e8d
Huan LI <zixia@zixia.net>
2017-04-30 19:53:57 +0800

    fix(ci) do not sudo npm

--- test/protractor.sh
+++ test/protractor.sh
@@ -4,7 +4,7 @@ set -e # http://stackoverflow.com/a/3474556/1123955
 APP_DIR="/tmp/brolog-angular-demo.$$"
 GIT_URL="https://github.com/zixia/brolog-angular-demo.git"
 
-sudo npm link
+npm link
 webdriver-manager update
 
 git clone "$GIT_URL" "$APP_DIR"
