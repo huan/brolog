50c5958d34c0d325af196879d15cdfc8aee62d73
Huan LI <zixia@zixia.net>
2018-04-06 10:23:09 +0800

    code clean

--- src/config.ts
+++ src/config.ts
@@ -8,13 +8,16 @@ try {
   // ignore
 }
 
-if (!pkg) {
-  try {
-    pkg = require('../../package.json')
-  } catch (e) {
-    // ignore
-  }
-}
+// Comment the following code out because
+// the Brolog will live in /bundles directory
+//
+// if (!pkg) {
+//   try {
+//     pkg = require('../../package.json')
+//   } catch (e) {
+//     // ignore
+//   }
+// }
 
 let VERSION = 'unknown'
 if (pkg) {
