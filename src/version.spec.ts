b4e94a63f147e189ea5c86b5e97fba3fe2fd2729
Huan (李卓桓) <zixia@zixia.net>
2020-06-17 09:23:17 +0000

    test

--- src/version.spec.ts
+++ src/version.spec.ts
@@ -19,14 +19,13 @@
  *   limitations under the License.
  *
  */
-import test  from 'blue-tape'
+const t = require('tap')  // tslint:disable:no-shadowed-variable
 
 import {
   VERSION,
-  GIT_COMMIT_HASH,
 }                   from './version'
 
-test('Make sure the VERSION is fresh in source code', async (t) => {
+t.test('Make sure the VERSION is fresh in source code', (t: any) => {
   t.equal(VERSION, '0.0.0', 'version should be 0.0.0 in source code, only updated before publish to NPM')
-  t.equal(GIT_COMMIT_HASH, '', 'git commit hash should be empty')
+  t.end()
 })
--- src/version.ts
+++ src/version.ts
@@ -19,4 +19,3 @@
  */
 
 export const VERSION = '0.0.0'
-export const GIT_COMMIT_HASH = ''
