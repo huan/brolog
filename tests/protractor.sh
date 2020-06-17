100414de36fccd6901f46ffd54f6c94a8c08a365
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-17 13:47:41 +0800

    install pack for e2e

--- tests/protractor.sh
+++ tests/protractor.sh
@@ -4,7 +4,8 @@ set -e # http://stackoverflow.com/a/3474556/1123955
 E2E_TESTING_DIR="/tmp/brolog-angular-demo.$$"
 GIT_URL="https://github.com/huan/brolog-angular-demo.git"
 
-npm link
+npm run dist
+npm pack
 
 # https://github.com/angular/webdriver-manager/issues/307#issuecomment-377684918
 webdriver-manager update --gecko=false
@@ -13,7 +14,7 @@ git clone "$GIT_URL" "$E2E_TESTING_DIR"
 
 cd "$E2E_TESTING_DIR"
 npm install
-npm link brolog
+npm install ../brolog-*.*.*.tgz
 npm start
 cd -
 
